71b9b7fce9eab780bf19fb2d23cc38ca01acec82
xi Wang <vincent_wangxi@126.com>
2018-05-18 13:48:35 +0800

    socket

---
+++ src/main/scala/com/imooc/spark/NetworkWoldCount.scala
@@ -0,0 +1,29 @@
+package com.imooc.spark
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * User: searainbow 
+  */
+object NetworkWoldCount {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
+
+    /**
+      * 创建StreamingContext需要两个参数：SparkConf和batch interval
+      */
+    val ssc = new StreamingContext(sparkConf, Seconds(5))
+
+    val lines = ssc.socketTextStream("localhost", 6789)
+
+    val result = lines.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
+
+    result.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+
+  }
+
+}
